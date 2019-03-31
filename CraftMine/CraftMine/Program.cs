b07f3fc86b8908e498c4072a2cddebec6a9bf1fe
Safernan12 <safernandez1@miuandes.cl>
2019-03-31 20:28:07 -0300

    Started the program's read file function

--- CraftMine/CraftMine/Program.cs
+++ CraftMine/CraftMine/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace CraftMine
 {
@@ -6,7 +8,15 @@ namespace CraftMine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello and Welcome to CraftMine, a Notch above for your Map Crafting");
+            Console.WriteLine("Please type the name of the .block file to Read (for example: marvin.blocks");
+
+            string firstFile = Console.ReadLine();
+
+            using (FileStream fs = File.Open(firstFile, FileMode.Open))
+            {
+
+            }
         }
     }
 }
