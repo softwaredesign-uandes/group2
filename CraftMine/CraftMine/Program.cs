d9e56b750d8abd7a2d215bf843b056ab157697ba
Safernan12 <safernandez1@miuandes.cl>
2019-03-31 21:02:36 -0300

    Program now read files

--- CraftMine/CraftMine/Program.cs
+++ CraftMine/CraftMine/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
@@ -9,13 +10,24 @@ namespace CraftMine
         static void Main(string[] args)
         {
             Console.WriteLine("Hello and Welcome to CraftMine, a Notch above for your Map Crafting");
-            Console.WriteLine("Please type the name of the .block file to Read (for example: marvin.blocks");
+            Console.WriteLine("Please type the name of the .block file to Read (for example: marvin.blocks)");
 
-            string firstFile = Console.ReadLine();
-
-            using (FileStream fs = File.Open(firstFile, FileMode.Open))
+            string path = Console.ReadLine();
+            
+            try
             {
+                string[] lines = File.ReadAllLines(path, Encoding.UTF8);
 
+                foreach (string line in lines)
+                {
+
+                    Console.WriteLine(line);
+                }
+                Console.Read();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("File not Found", e.ToString());
             }
         }
     }
