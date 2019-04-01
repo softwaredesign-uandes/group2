f24645bd15083d9099233157b5a55027f18bb296
Francisco Alvarez <s2globalmail@gmail.com>
2019-04-01 13:25:33 -0300

    Information can be received by the user, through the console, about the file.

--- CraftMine/CraftMine/Program.cs
+++ CraftMine/CraftMine/Program.cs
@@ -19,18 +19,69 @@ namespace CraftMine
             {
                 string[] lines = File.ReadAllLines(path, Encoding.UTF8);
                 String[][] data = new String[lines.Length][];
+                Console.WriteLine("Loading file, this may take a moment.");
                 for (int i=0; i<lines.Length; i++)
                 {
                     data[i] = lines[i].Split();
-                    foreach (string pnt in data[i])
-                    {
-                        Console.Write(pnt + ' ');
-                    }
-                    Console.WriteLine();
                 }
                 Console.WriteLine(path + " file received.");
-                Console.WriteLine("Press Enter key to close...");
-                Console.Read();
+                Console.WriteLine();
+                Console.WriteLine("Enter id to be checked.");
+                string idText = Console.ReadLine();
+                int id = 0;
+                if(Int32.TryParse(idText, out id)) {
+                    Console.WriteLine();
+                    Console.WriteLine("Press x for <x> coordinates.");
+                    Console.WriteLine("Press y for <y> coordinates.");
+                    Console.WriteLine("Press z for <z> coordinates.");
+                    if (path.Equals("zuck_small.block"))
+                    {
+                        Console.WriteLine("Press c for cost.");
+                        Console.WriteLine("Press v for value.");
+                        Console.WriteLine("Press r for rock_tonnes.");
+                        Console.WriteLine("Press o for ore_tonnes.");
+                    }
+                    else if (path.Equals("marvin.blocks"))
+                    {
+                        Console.WriteLine("Press t for tonn.");
+                        Console.WriteLine("Press a for au[ppm].");
+                        Console.WriteLine("Press u for cu %.");
+                        Console.WriteLine("Press p for proc_profit.");
+                    }
+                    string cord = Console.ReadLine();
+
+                    Console.WriteLine();
+                    Console.Write("Result: ");
+                    if (cord.Equals("x"))
+                    {
+                        Console.Write(data[id][1]);
+                    }
+                    else if (cord.Equals("y"))
+                    {
+                        Console.Write(data[id][2]);
+                    }
+                    else if (cord.Equals("z"))
+                    {
+                        Console.Write(data[id][3]);
+                    }
+                    else if (cord.Equals("c") || cord.Equals("t"))
+                    {
+                        Console.Write(data[id][4]);
+                    }
+                    else if (cord.Equals("v") || cord.Equals("a"))
+                    {
+                        Console.Write(data[id][5]);
+                    }
+                    else if (cord.Equals("r") || cord.Equals("u"))
+                    {
+                        Console.Write(data[id][6]);
+                    }
+                    else if (cord.Equals("o") || cord.Equals("p"))
+                    {
+                        Console.Write(data[id][7]);
+                    }
+                    Console.Read();
+                }
             }
             catch (Exception e)
             {
