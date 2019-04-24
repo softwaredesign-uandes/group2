24988e262896ab1fbee6c1983c470da4062464ae
Safernan12 <safernandez1@miuandes.cl>
2019-04-23 23:58:40 -0400

    Test developed, status red

---
+++ CraftMine/Quiz6SantiagoFernandez/Quiz6SantiagoFernandez/UnitTest.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Quiz6SantiagoFernandez
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SumTest()
+        {
+            //arrange
+            string listOfNumbers = "1,3,2,-1";
+            string delimeter = ",";
+            int expectedAnswer = 6;
+
+            TestSum testSum = new TestSum();
+
+            //act
+            int answer = testSum.sum();
+
+            //assert
+            Assert.AreEqual(expectedAnswer, answer);
+        }
+    }
+}
