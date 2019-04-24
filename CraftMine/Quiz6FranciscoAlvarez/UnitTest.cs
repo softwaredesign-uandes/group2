76f11ed9ef0ace62a3b0e3772e9e67e82758b1ec
Francisco Alvarez <s2globalmail@gmail.com>
2019-04-23 23:46:01 -0400

    Quiz project created, testAdd test created, throws RED due to lack of method.

--- CraftMine/Quiz6FranciscoAlvarez/UnitTest.cs
+++ CraftMine/Quiz6FranciscoAlvarez/UnitTest.cs
@@ -8,8 +8,18 @@ namespace Quiz6FranciscoAlvarez
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void testAdd()
         {
+            //arange
+            string numbers = "1,2,3";
+            int sum = 6;
+
+            //act
+            int final = Add(numbers);
+
+            //assert
+            Assert.AreEqual(sum, final);
+
         }
     }
 }
